a3cf4696c6dade00bde9ca990e54720b69271984
mparaiso <aikah@free.fr>
2013-03-27 16:00:05 +0100

    ShortenModelType

--- src/Mparaiso/Shortener/Form/ShortenModelType.php
+++ src/Mparaiso/Shortener/Form/ShortenModelType.php
@@ -6,13 +6,22 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Form\AbstractType;
 
-class ShortenModelType extends AbstractType {
-	public function buildForm(FormBuilderInterface $builder, array $options){
-		$builder->add("original","text",array("required"=>true,"constraints"=>array(new Assert\Length(array("min"=>5)),new Assert\Url())))
-		->add("custom","text",array("required"=>false,"constraints"=>array(new Assert\Length(array('min'=>4)))));
-	}
-	
-	function getName(){
-		return "ShortenModel";
-	}
+class ShortenModelType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add("original", "text",
+            array("required"    => TRUE,
+                  "constraints" => array(
+                      new Assert\Length(array("min" => 5)), new Assert\Url())))
+            ->add("custom", "text",
+            array("required"    => FALSE,
+                  "constraints" => array(
+                      new Assert\Length(array('min' => 4)))));
+    }
+
+    function getName()
+    {
+        return "ShortenModel";
+    }
 }
